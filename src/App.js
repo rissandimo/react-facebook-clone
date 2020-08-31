655eff740c9054af11a9e0c5397905b59a936987
= <nassiromid@gmail.com>
2020-08-31 11:52:49 -0700

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
 
+// Componets
 import Header from './Header';
+import Sidebar from './Sidebar';
+
 function App() {
   return (
     // BEM naming convention
     <div className="app">
 
-      {/* Header */}
       <Header />
 
-      {/* App body */}
-        {/* Sidebar */}
+      <div className="app__body">
+        <Sidebar />
         {/* feed */}
         {/* widgets */}
+
+      </div>
     
 
     </div>
---
+++ src/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// Components
+import SidebarRow from './SidebarRow';
+
+// Materialize
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags';
+import ChatIcon from '@material-ui/icons/Chat';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
+
+import './Sidebar.css';
+function Sidebar(){
+    return(
+        <div className="sidebar">
+            <SidebarRow src='https://avatars1.githubusercontent.com/u/5117934?s=460&u=b23a7d00e05bb10509a3ca7d8db9d3f1b6333e94&v=4' title='Omid Nassir'/>
+            <SidebarRow 
+              Icon={LocalHospitalIcon}
+               title='COVID-19 Information Centr' />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Friends" />
+            <SidebarRow Icon={ChatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Marketplace" />
+        </div>
+    );
+}
+
+export default Sidebar;
